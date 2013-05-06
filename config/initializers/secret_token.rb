34c90af90a537cab3cbf2f0061b07eea1bda7615
PC Dhulipalla <pchaitanya@zendesk.com>
2013-05-06 07:24:57 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeetupFun::Application.config.secret_token = '1d2e78743e02626e73d3c3d8bd08bd7536e27d6ead53ab314bff75535efade709713be5a398853b1d89a707f45de7595ff601c98ead6ebdc5f01b460c16e1653'
