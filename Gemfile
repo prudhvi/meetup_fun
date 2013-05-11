71b3b9da6c32beb19375c748fd837dbbab17d7da
Prudhvi <prudhvi.d123@gmail.com>
2013-05-10 21:44:10 -0700

    using pg and displaying group names when user logs in

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'rails', '3.2.13'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
+gem 'pg'
 
 
 # Gems used only for assets and not required
@@ -23,6 +23,8 @@ end
 gem 'jquery-rails'
 
 gem 'omniauth-meetup'
+
+gem 'meetup', :git => 'git@github.com:prudhvi/meetup.git'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,16 @@
 class SessionsController < ApplicationController
 
   def index
-
+    if current_user
+      @groups = Meetup::Groups.new(current_user.uid, session[:token]).all
+    end
   end
 
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["meetup"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
+    session[:token] = auth["credentials"]["token"]
     redirect_to root_url, :notice => "Signed in!"
   end
 
