697d6b53b4ce67416c6b68c6a622bead1f9bc58f
Prudhvi <prudhvi.d123@gmail.com>
2013-05-20 09:46:08 -0700

    Using postgres in development

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :name, :provider, :uid
 
   def self.find_or_create_with_omniauth(auth)
-    unless user = find_by_provider_and_uid(auth["provider"], auth["uid"])
+    unless user = find_by_provider_and_uid(auth["provider"], auth["uid"].to_s)
       user = create! do |user|
         user.provider = auth["provider"]
         user.uid = auth["uid"]
